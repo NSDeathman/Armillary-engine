ef9ca40ae14fbf1b43f6d1841bb6999d1ceb9d32
NSDeathman <forest.kir.325@gmail.com>
2026-01-18 00:44:20 +0300

    Комментарий

--- Source/Core/CoreMacros.h
+++ Source/Core/CoreMacros.h
@@ -1,7 +1,15 @@
+///////////////////////////////////////////////////////////////
 #pragma once
-
+///////////////////////////////////////////////////////////////
+// Core Library Macros
+//
+// Usage:
+//   CORE_BEGIN
+//     class CORE_API MyClass { ... };
+//   CORE_END
 #define CORE_BEGIN                                                                                                     \
 	namespace Core                                                                                                     \
 	{                                                                                                                  \
 
 #define CORE_END }
+///////////////////////////////////////////////////////////////
