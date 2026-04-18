4a9bb5821f2ac4bf4d5e8f8f0106c554952d3897
NSDeathman <forest.kir.325@gmail.com>
2026-04-19 01:03:41 +0400

    К проекту подключен SDL

--- Source/Engine/Engine.cpp
+++ Source/Engine/Engine.cpp
@@ -22,14 +22,51 @@ namespace Armillary
 	{
 		LOG_INFO("Initializing CEngine...");
 		LOG_INFO("CEngine initialized successfully");
-		return true;
+		
+
+		if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		{
+			LOG_ERROR("SDL could not initialize! SDL_Error: " + std::string(SDL_GetError()));
+			return false;
+		}
+
+		// Создание окна
+		SDL_Window* testWindow = SDL_CreateWindow(
+			"Armillary Engine - SDL2 Test",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			800, 600,
+			SDL_WINDOW_SHOWN
+		);
+
+		if (testWindow == nullptr)
+		{
+			LOG_ERROR("Window could not be created! SDL_Error: " + std::string(SDL_GetError()));
+			return false;
+		}
+
+		LOG_INFO("SDL2 initialized and window created successfully!");
+
+		// Задержка 3 секунды
+		SDL_Delay(3000);
+
+		// Уничтожение окна и выход
+		SDL_DestroyWindow(testWindow);
+		SDL_Quit();
+
+		return true; // или false, если вы хотите сразу завершить программу после теста
 	}
 
 	void Engine::Run()
 	{
 		LOG_INFO("CEngine main loop started");
 		std::cout << "Press Enter to exit..." << std::endl;
-		std::cin.get();
+		m_bIsRunning = true;
+		while (m_bIsRunning)
+		{
+			std::cin.get();
+			m_bIsRunning = false;
+		}
 		LOG_INFO("CEngine main loop finished");
 	}
 
--- Source/Engine/pch.h
+++ Source/Engine/pch.h
@@ -3,3 +3,5 @@
 #ifdef _WIN32
 #include <windows.h>
 #endif
+
+#include <SDL\SDL.h>
